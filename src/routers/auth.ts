963a95d11b08a0ad3acf17d239bf2d158a317608
emmanueluwa <emmanuel@fulodev.com>
2024-07-01 10:38:15 +0100

    adding validation for tokens and userIds upon registering

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,12 +1,15 @@
 import { create, verifyEmail } from "#/controllers/user";
 import { validate } from "#/middleware/validator";
-import { CreatingUserSchema } from "#/utils/validationSchema";
+import {
+  CreatingUserSchema,
+  EmailVerificationBody,
+} from "#/utils/validationSchema";
 import { Router } from "express";
 
 const router = Router();
 
 router.post("/create", validate(CreatingUserSchema), create);
 
-router.post("/verify-email", verifyEmail);
+router.post("/verify-email", validate(EmailVerificationBody), verifyEmail);
 
 export default router;
